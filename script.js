37b61b647571673f633897eabd8b2a6db68ea627
Cory <coryfay97@gmail.com>
2020-10-23 20:24:34 -0700

    edited to include for loop with random generator

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-//settiing variables
-//var askLength = prompt("Please enter the desired length of the password: 8-128");
-//var characters = prompt("Please list the character types you would like to include: 'lowercase', 'uppercase', 'numeric', and/or 'special characters'");
+//global variables
+var generateBtn = document.querySelector("#generate");
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = [ "!", "#", "$", "%", "&", "(", ")", "*", "+","-", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"];
-//main function
-function passGen() {
-var askLength = prompt("Please enter the desired length of the password: 8-128");
-var characters = prompt("Please list the character types you would like to include: 'lowercase', 'uppercase', 'numeric', and/or 'special characters'");
-  if (askLength >= 8 && askLength <= 128){
-    var realLength = Number(askLength); 
-    console.log(realLength);
-  } else (askLength);
- }  
+
+//Initial questions
+  var promptLength = prompt("Please select a password length between 8-128: ");
+  /*var conLowercase =  confirm("Would you like to include 'lowercase'?");
+  var conUppercase = confirm("Would you like to include 'uppercase'?");
+  var conNumbers =  confirm("Would you like to include 'numbers'?");
+  var conSpecialCharacters = confirm("Would you like to include 'special characters'?");
+  */
+
+  // Write password to the #password input
+function writePassword() {
+  //var password = generatePassword();
+  var i;
+  for (var i = 0; i < promptLength; i++){
+  var passwordText = document.querySelector("#password").textContent += lowercase[Math.floor(Math.random()*lowercase.length)];
+  }
+  passwordText.value = password;
+  
+
+}
 
 
-  //function passGen() {
-  //  document.getElementById("password-box").innerHTML = "Hello World";
-        
-  //}
-  //console.log(lowercase[Math.floor(Math.random()*realLength)]);
+    
 
-  //for (i=0; i<realLength; i++)
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
